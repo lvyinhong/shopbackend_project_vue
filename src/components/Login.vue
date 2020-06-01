033dde221763818eb413ee8e26f40421e05da9bc
吕尹红 <yinhong.lv@foxmail.com>
2020-06-01 22:03:02 +0800

    完成了登陆功能

---
+++ src/components/Login.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!--头像区域-->
+      <div class="avatar_box">
+        <img
+          src="../assets/logo.png"
+          alt=""
+        >
+      </div>
+      <!--登陆表单区域-->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        :rules="loginFormRules"
+        label-width="0px"
+        class="login_form"
+      >
+        <!--用户名区域-->
+        <el-form-item prop="username">
+          <el-input
+            v-model="loginForm.username"
+            prefix-icon="iconfont icon-user"
+          ></el-input>
+        </el-form-item>
+        <!--密码区域-->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="iconfont icon-password"
+          ></el-input>
+        </el-form-item>
+        <!--按钮区域-->
+        <el-form-item class="btns">
+          <el-button
+            type="primary"
+            @click="login"
+          >登陆</el-button>
+          <el-button
+            type="info"
+            @click="resetLoginForm"
+          >重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      // 这是登陆表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      // 这是表单的验证规则对象
+      loginFormRules: {
+        // 验证用户名是否合法
+        username: [
+          { required: true, message: '请输入登陆名称', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在3到10个字符', trigger: 'blur' }
+        ],
+        // 验证密码是否合法
+        password: [
+          { required: true, message: '请输入登陆密码', trigger: 'blur' },
+          { min: 6, max: 30, message: '长度在6到30个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 点击重置按钮，重置登陆表单
+    resetLoginForm() {
+      console.log(this)
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        console.log(valid)
+        if (!valid) { return }
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        console.log(res)
+        if (res.meta.status !== 200) {
+          return this.$message.error('登陆失败')
+        }
+        this.$message.success('登陆成功')
+        // 1、将登陆成功之后的 token 保存到客户端的 sessionStorage中
+        // 1.1 项目中除了登陆之外的其他API接口，必须在登陆之后才能访问
+        // 1.2 token 只应在当前玩转打开期间生效，所以将 token 保存在sessionStorage中
+        window.sessionStorage.setItem('token', res.data.token)
+        this.$router.push('/home')
+        // 2、通过编程式导航跳转到后台主页，路由地址是 /home
+      })
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eeeeee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #dddddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #ffffff;
+    img {
+      height: 100%;
+      width: 100%;
+      border-radius: 50%;
+      background-color: #eeeeee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 将弹框组件挂载到vue的原型属性
+Vue.prototype.$message = Message
